20a65da0adb7675d2d0cfe1e627b9f8b6797c1b0
Mike Wasielewski <mikewaz17@gmail.com>
2015-06-04 22:54:36 -0400

    working on best practices

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -9,33 +9,34 @@ public class WorstPractices extends Object {
 	/************************************************
 	 *	ASSIGNMENT:
 	 *	Fix code and comments below this block
+	 *
 	/************************************************/
 
 	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
+	    int magicNumber = WorstPractices.animals(false);
+	    magicNumber *= 5;
+	        if (magicNumber > 18) {
+	            while(magicNumber > 0)
 	{
-	MAGIC_NUMBER--;
-	}
-	}
+	    magicNumber--;
+	        }
+	    }
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
-		/*
-			Start off with one of these
-		*/
-	int aInt = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
-}
+	/* animals
+	   this method takes in a single parameter, yeswellheresthething.
+	   Using a very elaborate and complex algorithm, it calculate a magic number
+	   yeswellheresthething: a seed which helps generate the magic number
+	   returns: a magical number */
+	private static int animals(boolean yeswellheresthething) {
+       // Start off with one of these
+		int aInt = yeswellheresthething ? 34 : 21;
+		float sparklesfairy = .5f;
+		    for (int brown = 0; brown < aInt; brown++) { 
+			sparklesfairy *= aInt;
+		    } 
+		        return (int) sparklesfairy * aInt;
+	}
 
 	/************************************************
 	 *	ASSIGNMENT
